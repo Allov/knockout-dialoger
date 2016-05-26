ff87369c7bdc57f9dfefa768fbff9147d18b436d
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-26 16:43:30 -0400

    koco2...

--- dist/dialoger-ui.js
+++ dist/dialoger-ui.js
@@ -4,16 +4,16 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _dialoger = require('dialoger');
+var _kocoDialoger = require('koco-dialoger');
 
-var _dialoger2 = _interopRequireDefault(_dialoger);
+var _kocoDialoger2 = _interopRequireDefault(_kocoDialoger);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var ViewModel = function ViewModel() /*componentInfo*/{
     var self = this;
 
-    self.dialoger = _dialoger2.default;
+    self.dialoger = _kocoDialoger2.default;
 }; // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
